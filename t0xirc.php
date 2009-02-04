bb4283bb91b6f488fec618e4d372302ea88b9a94
Elan Ruusamäe <glen@pld-linux.org>
2009-02-04 11:32:56 +0000

    - add verbose flag, add usage display
    Changed files:
        t0xirc.php -> 1.5

--- t0xirc.php
+++ t0xirc.php
@@ -3,21 +3,59 @@
 /*
  nagiosalert.php 2006 Mikael Fridh <mikael.fridh@ongame.com>
  Telnets to an eggdrop and .say's messages to a channel.
- Modified and cleaned up by Elan Ruusam‰e <glen@pld-linux.org>
+ Modified and cleaned up by Elan Ruusam√§e <glen@pld-linux.org>
 */
 
 error_reporting(E_ALL & ~E_NOTICE);
-require "/usr/share/php/t0xirc.php";
+define('PROGRAM', basename($argv[0]));
 
-$opt = getopt("u:p:h:P:c:m:");
+function usage() {
+	$PROGRAM = PROGRAM;
+	fwrite(STDERR, "Usage:
+    {$PROGRAM} -u USER -p PASSWORD -h HOSTNAME -P PORT [-c CHANNEL] [-m MESSAGE]
 
-$mybot =& new t0xirc_bot($opt['u'], $opt['p'], $opt['h'], $opt['P']);
-$mybot->connect() or die("Unable to connect\n");
+Connects to an eggdrop and .say's messages to a channel.
+If CHANNEL is omited bot's default channel is used.
+if Message is omited, message is read from STDIN.
 
-printf("Connected to %s default chan %s.\n", $mybot->bot_nick, $mybot->channel['name']);
+");
+}
+
+require_once '/usr/share/php/t0xirc.php';
+
+$opt = getopt("u:p:h:P:c:m:v");
+if (empty($opt['u']) || empty($opt['p']) || empty($opt['h']) || empty($opt['P'])) {
+	usage();
+	exit(1);
+}
+
+$verbose = isset($opt['v']);
+
+$bot =& new t0xirc_bot($opt['u'], $opt['p'], $opt['h'], $opt['P']);
+if ($verbose) {
+	echo "Connecting to {$opt['h']}:{$opt['P']}\n";
+}
+if (!$bot->connect()) {
+	fwrite(STDERR, "Unable to connect to {$opt['h']}:{$opt['P']}!\n");
+	exit(1);
+}
+if ($verbose) {
+	echo "Connected!\n";
+}
+
+if (empty($opt['c'])) {
+	$opt['c'] = $bot->channel['name'];
+	if ($verbose) {
+		printf("Connected to %s default channel %s.\n", $bot->bot_nick, $bot->channel['name']);
+	}
+} else {
+	if ($verbose) {
+		printf("Connected to %s, channel: %s.\n", $bot->bot_nick, $bot->channel['name']);
+	}
+}
 
 if ($opt['m']) {
-	$mybot->say($opt['m'], $opt['c']);
+	$bot->say($opt['m'], $opt['c']);
 } else {
 	// read from stdin
 	$fp = STDIN;
@@ -26,10 +64,8 @@ if ($opt['m']) {
 		if (!$line) {
 			break;
 		}
-		$mybot->say($line, $opt['c']);
+		$bot->say($line, $opt['c']);
 	}
 }
 
-$mybot->disconnect();
-
-?>
+$bot->disconnect();
